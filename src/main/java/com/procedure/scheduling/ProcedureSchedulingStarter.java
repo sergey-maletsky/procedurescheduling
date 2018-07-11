78d1bc162b5dbc1f9a5ef825aa15878673a68300
sergey_maletsky <sergey.maletsky@firstlinesoftware.com>
2018-07-11 16:43:48 +0300

    1. added new gradle tasks for using the docker and fixed old one 2. extend app entry class to deploy on tomcat server

--- src/main/java/com/procedure/scheduling/ProcedureSchedulingStarter.java
+++ src/main/java/com/procedure/scheduling/ProcedureSchedulingStarter.java
@@ -2,9 +2,11 @@ package com.procedure.scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProcedureSchedulingStarter {
+public class ProcedureSchedulingStarter extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(ProcedureSchedulingStarter.class, args);
     }
